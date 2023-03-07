0fdaca7435f503a919e3e9a4848b6058a06ec55d
Mithun R Ankush <121188000+MithunRankush@users.noreply.github.com>
2023-03-07 16:33:59 +0530

    Delete wheelsformoney - Copy directory

--- wheelsformoney - Copy/jsp-wfm/src/main/java/com/jsp/jspwfm/Exception/MerchantAlreadyExitsException.java
+++
@@ -1,9 +0,0 @@
-package com.jsp.jspwfm.Exception;
-
-public class MerchantAlreadyExitsException extends 	Exception{
-	public MerchantAlreadyExitsException()
-	{
-		super("Merchant Already Exists");
-	}
-
-}
